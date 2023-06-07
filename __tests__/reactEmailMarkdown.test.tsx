66571ac27d4d85d4e1d54ff94adb0ca35b3bc09b
lordelogos <paulehiks@gmail.com>
2023-06-07 16:00:18 +0100

    feat: added tests for ReactEmailMarkdown component

---
+++ __tests__/reactEmailMarkdown.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@react-email/render";
+import { ReactEmailMarkdown } from "../src/components";
+
+describe("ReactEmailMarkdown component renders correctly", () => {
+  it("renders the markdown in the correct format for browsers", () => {
+    const actualOutput = render(
+      <ReactEmailMarkdown markdown={`# Hello, World!`} />
+    );
+    expect(actualOutput).toMatchInlineSnapshot(
+      `"<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><div><Section><Heading as="h1" style="font-weight:500;padding-top:20;font-size:2.5rem">Hello, World!</Heading></Section></div>"`
+    );
+  });
+});
