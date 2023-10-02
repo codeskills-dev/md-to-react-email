05de25a79f2176303d24ebdcac0d129b0f43fc20
lordelogos <paulehiks@gmail.com>
2023-10-02 02:03:47 +0100

    wip: remove data-attr props from parser and utils

--- src/parser.ts
+++ src/parser.ts
@@ -5,14 +5,8 @@ import { initRenderer } from "./utils";
 export class MarkdownParser {
   private readonly renderer: RendererObject;
 
-  constructor({
-    customStyles,
-    withDataAttr,
-  }: {
-    customStyles?: StylesType;
-    withDataAttr?: boolean;
-  }) {
-    this.renderer = initRenderer({ customStyles, withDataAttr });
+  constructor({ customStyles }: { customStyles?: StylesType }) {
+    this.renderer = initRenderer({ customStyles });
   }
 
   parse(markdown: string) {
