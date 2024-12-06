ab516a46d4a5e16e6d6faaf75b1bcb87e330a7b0
gabriel miranda <gabrielmfern@outlook.com>
2024-12-06 11:49:23 -0300

    update rendering test

--- __tests__/emailMarkdown.test.tsx
+++ __tests__/emailMarkdown.test.tsx
@@ -3,8 +3,8 @@ import { render } from "@react-email/render";
 import { EmailMarkdown } from "../src";
 
 describe("ReactEmailMarkdown component renders correctly", () => {
-  it("renders the markdown in the correct format for browsers", () => {
-    const actualOutput = render(
+  it("renders the markdown in the correct format for browsers", async () => {
+    const actualOutput = await render(
       <EmailMarkdown
         markdown={`# Hello, World!
 ## Hi, World`}
@@ -16,7 +16,7 @@ describe("ReactEmailMarkdown component renders correctly", () => {
     );
 
     expect(actualOutput).toMatchInlineSnapshot(
-      `"<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><div><h1 style="font:700 23px / 32px &#x27;Roobert PRO&#x27;, system-ui, sans-serif">Hello, World!</h1><h2 style="background:url(&#x27;path/to/image&#x27;)">Hi, World</h2></div>"`
+      `"<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><!--$--><div><h1 style="font:700 23px / 32px &#x27;Roobert PRO&#x27;, system-ui, sans-serif">Hello, World!</h1><h2 style="background:url(&#x27;path/to/image&#x27;)">Hi, World</h2></div><!--/$-->"`
     );
   });
 });
